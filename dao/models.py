44765cdc39af70fd221e5ce9d7366abb6112f089
doom <phramov945@gmail.com>
2022-09-23 19:48:54 +0300

    password field removed from UserSchema

--- dao/models.py
+++ dao/models.py
@@ -26,7 +26,6 @@ class User(db.Model):
 class UserSchema(Schema):
     id = fields.Int()
     email = fields.Str()
-    password = fields.Str()
     name = fields.Str()
     surname = fields.Str()
 
