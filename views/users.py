0bd15002aace86e0310582992c746047aa15b8cd
doom <phramov945@gmail.com>
2022-09-23 19:47:30 +0300

    HTTP-method GET added

--- views/users.py
+++ views/users.py
@@ -3,8 +3,11 @@ from flask_restx import Resource, Namespace
 from container import user_service, auth_service
 from helpers.decorators import auth_required
 from utils import bytes_to_dict
+from dao.models import UserSchema
 
 user_ns = Namespace('user')
+
+user_schema = UserSchema()
 
 
 @user_ns.route('/')
@@ -12,7 +15,9 @@ user_ns = Namespace('user')
 class UserView(Resource):
     @auth_required
     def get(self):
-        return "", 200
+        token = request.headers['Authorization'].split("Bearer ")[-1]
+        user_instance = auth_service.get_user_by_token(token)
+        return user_schema.dump(user_instance), 200
 
     @auth_required
     def put(self):
