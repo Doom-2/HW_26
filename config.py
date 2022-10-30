dce1ec530f910307e0cbd8b047b83aea33ea4683
doom <phramov945@gmail.com>
2022-10-30 20:15:01 +0300

    SQLALCHEMY_DATABASE_URI modified

--- config.py
+++ config.py
@@ -1,9 +1,19 @@
 import base64
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv('.env', raise_error_if_not_found=False))
 
 
 class Config(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///./project_db.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{username}:{password}@{host}:{port}/{db_name}'.format(
+        username=os.getenv('POSTGRES_USER'),
+        password=os.getenv('POSTGRES_PASSWORD'),
+        host=os.getenv('POSTGRES_HOST', '127.0.0.1'),
+        port=int(os.getenv('POSTGRES_PORT', 5432)),
+        db_name=os.getenv('POSTGRES_DB')
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     TOKEN_EXPIRE_MINUTES = 15
